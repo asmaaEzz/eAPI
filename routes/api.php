1387b1e84c11239e2c36ec22806b6ff7034a198e
Asmaa Ezz <asmaaezzeldean1@gmail.com>
2021-01-04 01:00:44 +0200

    created models -a

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('/products','\App\Http\Controllers\ProductController');
+
+Route::group(['prefix'=>'products'],function (){
+    Route::apiResource('/{product}/review','\App\Http\Controllers\ReviewController');
+});
